9664b7ac493e91f75a6d33e65ac15f08e0297bf4
sugichan0116 <32512315+sugichan0116@users.noreply.github.com>
2019-10-24 09:09:19 +0900

    Minimap icon

---
+++ Assets/MinimapMarker.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UniRx;
+using System;
+using UniRx.Triggers;
+
+public class MinimapMarker : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        var manager = MinimapManager.Instance;
+        var parent = transform.parent;
+        var scaleOffset = transform.localScale;
+
+        Observable.EveryUpdate()
+            .Subscribe(_ =>
+            {
+                var position = manager.range.ClosestPoint(parent.position);
+                var isOutRange = (Vector2)parent.position != position;
+                var scaleRate = (isOutRange ? manager.outRangeScale : 1f);
+
+                transform.position = position;
+                transform.localScale = scaleOffset * scaleRate;
+            })
+            .AddTo(this);
+    }
+}
